5f90fe1962430b8f4390db0ad037e56cae5fd992
Jxk0be <JakeShoffner27@gmail.com>
2023-08-12 10:26:51 -0400

    feat: Fleshing out the add expenses page

--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -4,6 +4,46 @@ import Navbar from '../Components/Navbar';
 
 const Create = () => {
   const [checked, setChecked] = useState(false)
+  const [title, setTitle] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [category, setCategory] = useState('')
+  const [notes, setNotes] = useState('')
+  const [formObject, setFormObject] = useState(
+  {
+    "title": "",
+    "amount": null,
+    "category": "",
+    "notes": ""
+  })
+
+  const handleTitle = (e) => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  }
+
+  const handleAmount = (e) => {
+    e.preventDefault();
+    setAmount(e.target.value);
+  }
+
+  const handleCategory = (e) => {
+    e.preventDefault();
+    setCategory(e.target.value);
+  }
+
+  const handleNotes = (e) => {
+    e.preventDefault();
+    setNotes(e.target.value);
+  }
+
+  const handleFormObject = () => {
+    setFormObject({
+      "title": title,
+      "amount": amount,
+      "category": category,
+      "notes": notes
+    })
+  }
 
   return (
   <div className='w-full h-screen flex justify-center'>
@@ -12,23 +52,23 @@ const Create = () => {
       <h1>Add an Expense</h1>
       <div>
         <h1>Title</h1>
-        <input type='text' className='bg-red-50 mb-5'/>
+        <input type='text' className='bg-red-50 mb-5' value={title} onChange={(e) => handleTitle(e)}/>
 
         <h1>Amount</h1>
-        <input type='number' className='bg-red-50 mb-5'/>
-
+        <input type='number' className='bg-red-50 mb-5' value={amount} onChange={(e) => handleAmount(e)}/>
+        
         <h1>Category</h1>
-        <input type='text' className='bg-red-50 mb-5'/>
-
+        <input type='text' className='bg-red-50 mb-5' value={category} onChange={(e) => handleCategory(e)}/>
+        
         <h1>Notes</h1>
         <input type='checkbox' onChange={() => setChecked(!checked)} className={`bg-red-50 ${checked ? '' : 'mb-5'}`}/>
         {checked ?
           <div className='mb-5'>
-            <textarea />
+            <textarea value={notes} onChange={(e) => handleNotes(e)}/>
           </div> : 
           ''
         }
-        <h1><button className='bg-red-50 mb-5'>Add Expense</button></h1>
+        <h1><button className='bg-red-50 mb-5' onClick={() => handleFormObject()}>Add Expense</button></h1>
         {/*
           Expense Title (input field)
           Expense Amount (Input field, but have a dropdown to enter yen vs USD)
