9723b0aebc4eac8d0a885f59fc7ed3762ee164ac
Jxk0be <JakeShoffner27@gmail.com>
2023-08-13 00:45:06 -0400

    refactor: Cleanup some unneeded code

--- src/Pages/Expenses.jsx
+++ src/Pages/Expenses.jsx
@@ -26,8 +26,8 @@ const Expenses = () => {
       <Navbar />
       <div className='pt-[65px] max-w-[1500px] px-6 w-full flex flex-col items-center text-xl font-semibold'>
         <h1 className='text-3xl underline'>Expenses</h1>
-        {expenses.map((exp, index) => (
-          <h1 key={index}><Link to={exp.id}>{exp.title}</Link></h1>
+        {expenses.map(exp => (
+          <h1 key={exp.id}><Link to={exp.id}>{exp.title}</Link></h1>
         ))}
       </div>
     </div>
