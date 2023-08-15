85817245ea443db8bca262fe18139f28b9094826
Jxk0be <JakeShoffner27@gmail.com>
2023-08-15 18:03:32 -0400

    style: Add loading symbol

--- src/Pages/Expenses.jsx
+++ src/Pages/Expenses.jsx
@@ -3,9 +3,11 @@ import Navbar from '../Components/Navbar';
 import { Link } from "react-router-dom";
 import { db } from '../firebase';
 import { query, collection, onSnapshot } from 'firebase/firestore';
+import { Icon } from '@iconify/react';
 
 const Expenses = () => {
   const [expenses, setExpenses] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const q = query(collection(db, 'expenses'))
@@ -17,6 +19,7 @@ const Expenses = () => {
 
       expensesArray.sort(function(a, b) { return b.date - a.date })
       setExpenses(expensesArray)
+      setLoading(!loading)
     })
     return () => unsubscribe()
   }, [])
@@ -26,16 +29,19 @@ const Expenses = () => {
       <Navbar />
       <div className='pt-[65px] max-w-[1500px] px-6 w-full flex flex-col items-center text-xl font-semibold'>
         <h1 className='text-3xl underline'>Expenses</h1>
-        {expenses.map(exp => (
-          <div key={exp.id} className='w-full flex justify-center items-center m-3'>
-            <Link to={exp.id} className='w-1/3 flex justify-center bg-blue-200 p-3 rounded-md'>
-              <div className='flex justify-between items-center w-full'>
-                <h1>{exp.title}</h1>
-                <h1>${exp.amount}</h1>
-              </div>
-            </Link>
-          </div>
-        ))}
+        {loading ? 
+          <Icon icon="line-md:loading-loop" className='text-[50px] mt-5' /> : 
+          expenses.map(exp => (
+            <div key={exp.id} className='w-full flex justify-center items-center m-3'>
+              <Link to={exp.id} className='w-1/3 flex justify-center bg-blue-200 p-3 rounded-md'>
+                <div className='flex justify-between items-center w-full'>
+                  <h1>{exp.title}</h1>
+                  <h1>${exp.amount}</h1>
+                </div>
+              </Link>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
